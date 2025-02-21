be7e06b873d4e2974c1e6af783ae28256b1eb94c
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-02-21 14:59:09 +0530

    Footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,59 @@
+import { SlHome } from "react-icons/sl";
+import { HiHome } from "react-icons/hi";
+import { IoMdHelpCircleOutline, IoMdHelpCircle } from "react-icons/io";
+import { VscAccount } from "react-icons/vsc";
+import { RiAccountCircleFill } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+interface FooterProps {
+    currPage: "home" | "help" | "account",
+    setCurrPage: (str: "home" | "help" | "account") => void
+}
+
+export default function Footer({
+    currPage,
+    setCurrPage
+}:FooterProps){
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currPage === "home") {
+      navigate("/");
+    } else {
+      navigate(`/${currPage}`);
+    }
+  }, [currPage, navigate]);
+
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-black flex justify-evenly items-center h-[50px]">
+      <FooterButton name="home" Active={HiHome} InActive={SlHome} currPage={currPage} setCurrPage={setCurrPage} />
+      <FooterButton name="help" Active={IoMdHelpCircle} InActive={IoMdHelpCircleOutline} currPage={currPage} setCurrPage={setCurrPage} />
+      <FooterButton name="account" Active={RiAccountCircleFill} InActive={VscAccount} currPage={currPage} setCurrPage={setCurrPage} />
+    </div>
+  );
+}
+
+interface FooterButtonProps {
+  Active: React.ElementType,
+  InActive: React.ElementType,
+  name: "home" | "help" | "account",
+  currPage: "home" | "help" | "account",
+  setCurrPage: (str: "home" | "help" | "account") => void,
+}
+
+function FooterButton({ Active, InActive, name, currPage, setCurrPage }: FooterButtonProps) {
+  return (
+    <div
+      className="flex flex-col items-center w-[33%] text-white text-sm cursor-pointer"
+      onClick={() => setCurrPage(name)}
+    >
+      {currPage === name ? <Active className="w-5 h-5" /> : <InActive className="w-5 h-5" />}
+      <div>{toTitleCase(name)}</div>
+    </div>
+  );
+}
+
+function toTitleCase(str: string) {
+  return str.replace("_", " ").replace(/(?:^|\s)\w/g, (match) => match.toUpperCase());
+}
